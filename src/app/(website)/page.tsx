c15808190b269862c8d205aed265f4ec02214ab9
sozibhossain <sozibhossain69@gmail.com>
2025-03-08 15:55:17 +0600

    Added Header

--- src/app/(website)/page.tsx
+++ src/app/(website)/page.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import TileSimulator from './tile-simulator/tile-simulator'
 import Navbar from '@/components/Shared/Header/navbar'
 
 function page() {
--- src/components/Shared/Header/navbar.tsx
+++ src/components/Shared/Header/navbar.tsx
@@ -44,11 +44,11 @@ export default function Navbar() {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
+            {/* <img
               alt=""
               src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
               className="h-8 w-auto"
-            />
+            /> */}
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -128,11 +128,11 @@ export default function Navbar() {
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
+              {/* <img
                 alt=""
                 src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
                 className="h-8 w-auto"
-              />
+              /> */}
             </a>
             <button
               type="button"
